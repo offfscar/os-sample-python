a95f01e8e10f9fbd07abd54ffdf5431fc710f9c2
Oscar Jimenez <offfscar@gmail.com>
2018-09-07 12:02:55 -0500

    se cambia path /hello

--- wsgi.py
+++ wsgi.py
@@ -4,9 +4,18 @@ application = Flask(__name__)
 @application.route("/hello", methods=['GET'])
 def hello():
     operator = request.args.get('operator')
-    num1 = request.args.get('num1')
-    num2 = request.args.get('num2')
-    return num1 + num2
+    num1 = int(request.args.get('num1'))
+    num2 = int(request.args.get('num2'))
+
+    if operator == 'add':
+        result = num1 + num2
+    elif operator == 'sub':
+        result = num1 - num2
+    elif operator == 'mul':
+        result = num1 * num2
+    elif operator == 'div':
+        result = num1 / num2
+    return 'El reultado es ' + result
 
 if __name__ == "__main__":
     application.run()
