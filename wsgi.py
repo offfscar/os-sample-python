d1c42df80fd175e888558f1fb7e0832b0e8c8508
Oscar Jimenez <offfscar@gmail.com>
2018-09-07 12:07:13 -0500

    se implementa calculator

--- wsgi.py
+++ wsgi.py
@@ -4,8 +4,8 @@ application = Flask(__name__)
 @application.route("/hello", methods=['GET'])
 def hello():
     operator = request.args.get('operator')
-    num1 = int(request.args.get('num1'))
-    num2 = int(request.args.get('num2'))
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
 
     if operator == 'add':
         result = num1 + num2
@@ -15,7 +15,7 @@ def hello():
         result = num1 * num2
     elif operator == 'div':
         result = num1 / num2
-    return 'El reultado es ' + result
+    return 'El reultado es ' + str(result)
 
 if __name__ == "__main__":
     application.run()
